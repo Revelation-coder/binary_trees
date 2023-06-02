a451c402a929bc8f7fd9238c9382cbaf562bd291
Revelation-coder <captainrevelation@gmail.com>
2023-06-02 02:00:54 -0700

    debugging like a pro

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -11,61 +11,61 @@
  */
 avl_t *avl_remove(avl_t *root, int value)
 {
-    if (root == NULL)
-        return NULL;
+	if (root == NULL)
+		return (NULL);
 
-    if (value < root->n)
-        root->left = avl_remove(root->left, value);
-    else if (value > root->n)
-        root->right = avl_remove(root->right, value);
-    else
-    {
-        if (root->left == NULL || root->right == NULL)
-        {
-            avl_t *temp = (root->left) ? root->left : root->right;
+	if (value < root->n)
+		root->left = avl_remove(root->left, value);
+	else if (value > root->n)
+		root->right = avl_remove(root->right, value);
+	else
+	{
+		if (root->left == NULL || root->right == NULL)
+		{
+			avl_t *temp = (root->left) ? root->left : root->right;
 
-            if (temp == NULL)
-            {
-                temp = root;
-                root = NULL;
-            }
-            else
-                *root = *temp;
+			if (temp == NULL)
+			{
+				temp = root;
+				root = NULL;
+			}
+			else
+				*root = *temp;
 
-            free(temp);
-        }
-        else
-        {
-            avl_t *temp = avl_minimum(root->right);
+			free(temp);
+		}
+		else
+		{
+			avl_t *temp = avl_minimum(root->right);
 
-            root->n = temp->n;
-            root->right = avl_remove(root->right, temp->n);
-        }
-    }
+			root->n = temp->n;
+			root->right = avl_remove(root->right, temp->n);
+		}
+	}
 
-    if (root == NULL)
-        return NULL;
+	if (root == NULL)
+		return (NULL);
 
-    root->height = 1 + max(avl_height(root->left), avl_height(root->right));
-    root->balance = avl_balance(root);
+	root->height = 1 + avl_max(avl_height(root->left), avl_height(root->right));
+	root->balance = avl_balance(root);
 
-    if (root->balance > 1 && avl_balance(root->left) >= 0)
-        return avl_rotate_right(root);
+	if (root->balance > 1 && avl_balance(root->left) >= 0)
+		return (avl_rotate_right(root));
 
-    if (root->balance > 1 && avl_balance(root->left) < 0)
-    {
-        root->left = avl_rotate_left(root->left);
-        return avl_rotate_right(root);
-    }
+	if (root->balance > 1 && avl_balance(root->left) < 0)
+	{
+		root->left = avl_rotate_left(root->left);
+		return (avl_rotate_right(root));
+	}
 
-    if (root->balance < -1 && avl_balance(root->right) <= 0)
-        return avl_rotate_left(root);
+	if (root->balance < -1 && avl_balance(root->right) <= 0)
+		return (avl_rotate_left(root));
 
-    if (root->balance < -1 && avl_balance(root->right) > 0)
-    {
-        root->right = avl_rotate_right(root->right);
-        return avl_rotate_left(root);
-    }
+	if (root->balance < -1 && avl_balance(root->right) > 0)
+	{
+		root->right = avl_rotate_right(root->right);
+		return (avl_rotate_left(root));
+	}
 
-    return root;
+	return (root);
 }
